c41cc58ac4fe2f485e75cdab526a3bb2ef39d052
Bohdan Fedirko <bohhhhdan@gmail.com>
2023-11-08 15:13:50 +0100

    IOS ifx

--- src/components/UI/WeatherBox/WeatherBox.tsx
+++ src/components/UI/WeatherBox/WeatherBox.tsx
@@ -15,7 +15,7 @@ export const WeatherBox = (props: any) => {
   const timeOnly = String(props.time).substring(11, 16);
   const dayOnly = String(props.time).substring(0, 10);
   const dateOnly = String(props.time).substring(8, 10);
-  const month = String(props.day).substring(5, 7);
+  const month = String(props.day).substring(0, 10);
   const day = String(props.day).substring(8, 10);
 
   return (
